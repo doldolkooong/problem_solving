cb612859d70b64c3ac310a9867815064f14b1801
안효원 <912110h@gmail.com>
2026-08-16 00:40:30 +0900

    수정본
    수정본

---
+++ 2. comprehensive game/2. comprehensive game/game/inputs.py
@@ -0,0 +1,15 @@
+import random
+from .constants import rock_sissor_paper
+
+def get_user_input() -> rock_sissor_paper:
+    user_input = input("가위, 바위, 보 중 하나를 선택하세요").strip()
+
+    if user_input not in rock_sissor_paper.__members__:
+        print("다시 선택해주세요..^^;;")
+        return get_user_input()
+
+    return rock_sissor_paper[user_input]
+
+
+if __name__ == "__main__":
+    print(get_user_input())
