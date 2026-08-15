a057f0538770b4d5afa1b7327836db798be26a0e
안효원 <912110h@gmail.com>
2026-08-16 00:40:51 +0900

    Delete 2. comprehensive game/2. comprehensive game directory

--- 2. comprehensive game/2. comprehensive game/game/prints.py
+++
@@ -1,6 +0,0 @@
-def print_game_result(result:str, user_value:str=None, computer_value:str=None) -> None:
-    if user_value and computer_value:
-        print(f"User 선택 : {user_value} \nComputer 선택 : {computer_value}")
-
-    print(f"게임 결과 :  {result}")
-
